5f25d486109426ad4017dcdb964be5316cf3db96
Forkhammer <forkhammer@gmail.com>
2023-11-28 22:03:50 +0500

    fix spec

--- frontend/board/src/app/app.component.spec.ts
+++ frontend/board/src/app/app.component.spec.ts
@@ -14,16 +14,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'board'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('board');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('board app is running!');
-  });
 });

82e2ecab840592b0c92520ffc39167d58c03402a
Forkhammer <forkhammer@gmail.com>
2025-05-26 14:10:33 +0500

    bump angular v18

--- frontend/board/src/app/app.module.ts
+++ frontend/board/src/app/app.module.ts
@@ -1,42 +1,45 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG,
+  HammerModule,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {CoreModule} from "./modules/core/core.module";
-import {UiModule} from "./modules/ui/ui.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { CoreModule } from './modules/core/core.module';
+import { UiModule } from './modules/ui/ui.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IndexPageComponent } from './components/index-page/index-page.component';
-import {environment} from "../environments/environment";
-import {KanbanModule} from "./modules/kanban/kanban.module";
-import {JWTInterceptor} from "./modules/core/interceptors/jwt.interceptor";
+import { environment } from '../environments/environment';
+import { KanbanModule } from './modules/kanban/kanban.module';
+import { JWTInterceptor } from './modules/core/interceptors/jwt.interceptor';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
-import {BootstrapUiModule} from "./modules/bootstrap-ui/bootstrap-ui.module";
-import * as Hammer from "hammerjs";
-
+import { BootstrapUiModule } from './modules/bootstrap-ui/bootstrap-ui.module';
+import * as Hammer from 'hammerjs';
 
 export class MyHammerConfig extends HammerGestureConfig {
-  override overrides = <any> {
-    swipe: { direction: Hammer.DIRECTION_ALL, velocity: 0.4, threshold: 20, },
+  override overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL, velocity: 0.4, threshold: 20 },
   };
 }
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    IndexPageComponent,
-    AuthPageComponent
-  ],
+  declarations: [AppComponent, IndexPageComponent, AuthPageComponent],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     CoreModule.forRoot({
-      apiUrl: environment.apiUrl
+      apiUrl: environment.apiUrl,
     }),
     UiModule,
     KanbanModule,
@@ -55,7 +58,7 @@ export class MyHammerConfig extends HammerGestureConfig {
       provide: HAMMER_GESTURE_CONFIG,
       useClass: MyHammerConfig,
     },
+    provideHttpClient(withInterceptorsFromDi()),
   ],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

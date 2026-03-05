cb642f62177ecdbdaed34e2309b854eba043656e
Forkhammer <forkhammer@gmail.com>
2026-03-05 19:29:49 +0500

    shortcuts

--- frontend/board/src/app/modules/kanban/components/bind-issue-modal/bind-issue-modal.component.ts
+++ frontend/board/src/app/modules/kanban/components/bind-issue-modal/bind-issue-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, ElementRef, inject, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, DestroyRef, ElementRef, inject, AfterViewInit, ViewChild, QueryList, ViewChildren } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { IssueService } from '../../services/issue.service';
 import { KanbanIssue } from '../../models/kanban-issue';
@@ -14,6 +14,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 })
 export class BindIssueModalComponent implements AfterViewInit {
   @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+  @ViewChildren('issueItem') issueItems!: QueryList<ElementRef<HTMLElement>>;
 
   modal = inject(NgbActiveModal);
   issueService = inject(IssueService);
@@ -22,6 +23,7 @@ export class BindIssueModalComponent implements AfterViewInit {
   searchQuery = '';
   issues: KanbanIssue[] = [];
   isLoading = false;
+  activeIndex = -1;
 
   private search$ = new Subject<string>();
 
@@ -31,6 +33,7 @@ export class BindIssueModalComponent implements AfterViewInit {
       distinctUntilChanged(),
       switchMap(query => {
         this.isLoading = true;
+        this.activeIndex = -1;
         if (!query.trim()) {
           this.isLoading = false;
           return of(null);
@@ -59,6 +62,28 @@ export class BindIssueModalComponent implements AfterViewInit {
 
   onSearchChange(query: string) {
     this.search$.next(query);
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (!this.issues.length) return;
+
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.activeIndex = Math.min(this.activeIndex + 1, this.issues.length - 1);
+      this.scrollActiveIntoView();
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.activeIndex = Math.max(this.activeIndex - 1, 0);
+      this.scrollActiveIntoView();
+    } else if (event.key === 'Enter' && this.activeIndex >= 0) {
+      event.preventDefault();
+      this.select(this.issues[this.activeIndex]);
+    }
+  }
+
+  private scrollActiveIntoView() {
+    const items = this.issueItems.toArray();
+    items[this.activeIndex]?.nativeElement.scrollIntoView({ block: 'nearest' });
   }
 
   select(issue: KanbanIssue) {

c040d1590e7001f7511902e1348689000bd9d297
Forkhammer <forkhammer@gmail.com>
2026-03-06 20:16:45 +0500

    di refactoring

---
+++ backend/di.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"main/config"
+	app_services "main/internal/app/services"
+	"main/internal/app/usecases"
+	"main/internal/infra/db/implementation"
+	"main/internal/infra/db/interfaces"
+	"main/internal/infra/gitlab"
+	"main/internal/infra/persistance/repo"
+	"main/internal/infra/persistance/spec"
+	column_spec "main/internal/infra/persistance/spec/column"
+	epic_spec "main/internal/infra/persistance/spec/epic"
+	group_spec "main/internal/infra/persistance/spec/group"
+	issue_spec "main/internal/infra/persistance/spec/issue"
+	issuebinding_spec "main/internal/infra/persistance/spec/issue_binding"
+	label_spec "main/internal/infra/persistance/spec/label"
+	project_spec "main/internal/infra/persistance/spec/project"
+	release_spec "main/internal/infra/persistance/spec/release"
+	sprint_spec "main/internal/infra/persistance/spec/sprint"
+	user_spec "main/internal/infra/persistance/spec/user"
+	"main/internal/infra/services"
+	"main/internal/interfaces/api/controllers"
+	"reflect"
+
+	"github.com/goioc/di"
+)
+
+func mustRegisterBean(beanID string, beanType reflect.Type) {
+	if _, err := di.RegisterBean(beanID, beanType); err != nil {
+		panic(fmt.Sprintf("failed to register bean %q: %v", beanID, err))
+	}
+}
+
+func mustRegisterBeanInstance(beanID string, beanInstance any) {
+	if _, err := di.RegisterBeanInstance(beanID, beanInstance); err != nil {
+		panic(fmt.Sprintf("failed to register bean instance %q: %v", beanID, err))
+	}
+}
+
+func mustRegisterBeanFactory(beanID string, scope di.Scope, beanFactory func(ctx context.Context) (any, error)) {
+	if _, err := di.RegisterBeanFactory(beanID, scope, beanFactory); err != nil {
+		panic(fmt.Sprintf("failed to register bean factory %q: %v", beanID, err))
+	}
+}
+
+func initDI() {
+	dbType := interfaces.DbType(config.Settings.DbType)
+
+	connection, err := implementation.GetConnectionByType(dbType, config.Settings)
+	if err != nil {
+		panic(err)
+	}
+
+	mustRegisterBeanInstance("connection", connection)
+	mustRegisterBeanInstance("config", config.Settings)
+	mustRegisterBeanInstance("db", connection)
+	mustRegisterBeanFactory("gitlab", di.Singleton, func(ctx context.Context) (any, error) {
+		return gitlab.NewGitlabClient(config.Settings.GitlabUrl, config.Settings.GitlabToken), nil
+	})
+
+	registerRepositories()
+	registerQueries(dbType)
+	registerServices()
+	registerUseCases()
+	registerControllers()
+
+	if err = di.InitializeContainer(); err != nil {
+		panic(err)
+	}
+}
+
+func registerRepositories() {
+	mustRegisterBean("AccountRepository", reflect.TypeFor[*repo.AccountRepository]())
+	mustRegisterBean("ColumnRepository", reflect.TypeFor[*repo.ColumnRepository]())
+	mustRegisterBean("GroupRepository", reflect.TypeFor[*repo.GroupRepository]())
+	mustRegisterBean("IssueRepository", reflect.TypeFor[*repo.IssueRepository]())
+	mustRegisterBean("KeyValueRepository", reflect.TypeFor[*repo.KeyValueRepository]())
+	mustRegisterBean("LabelRepository", reflect.TypeFor[*repo.LabelRepository]())
+	mustRegisterBean("ProjectRepository", reflect.TypeFor[*repo.ProjectRepository]())
+	mustRegisterBean("ReleaseRepository", reflect.TypeFor[*repo.ReleaseRepository]())
+	mustRegisterBean("TeamRepository", reflect.TypeFor[*repo.TeamRepository]())
+	mustRegisterBean("UserRepository", reflect.TypeFor[*repo.UserRepository]())
+	mustRegisterBean("SettingsRepository", reflect.TypeFor[*repo.SettingsRepository]())
+	mustRegisterBean("SprintRepository", reflect.TypeFor[*repo.SprintRepository]())
+	mustRegisterBean("IssueBindingRepository", reflect.TypeFor[*repo.IssueBindingRepository]())
+	mustRegisterBean("EpicRepository", reflect.TypeFor[*repo.EpicRepository]())
+}
+
+func registerQueries(dbType interfaces.DbType) {
+	mustRegisterBean("LabelQuery", reflect.TypeFor[*label_spec.LabelQueryImpl]())
+	mustRegisterBean("ColumnQuery", reflect.TypeFor[*column_spec.ColumnQueryImpl]())
+	mustRegisterBean("GroupQuery", reflect.TypeFor[*group_spec.GroupQueryImpl]())
+	mustRegisterBean("UserQuery", reflect.TypeFor[*user_spec.UserQueryImpl]())
+	mustRegisterBean("SprintQuery", reflect.TypeFor[*sprint_spec.SprintQueryImpl]())
+	mustRegisterBean("ProjectQuery", reflect.TypeFor[*project_spec.ProjectQueryImpl]())
+	mustRegisterBean("IssueBindingQuery", reflect.TypeFor[*issuebinding_spec.IssueBindingQueryImpl]())
+	mustRegisterBean("ReleaseQuery", reflect.TypeFor[*release_spec.ReleaseQueryImpl]())
+	mustRegisterBean("EpicQuery", reflect.TypeFor[*epic_spec.EpicQueryImpl]())
+	mustRegisterBean("CommonQuery", reflect.TypeFor[*spec.CommonQueryImpl]())
+
+	registerIssueQuery(dbType)
+}
+
+func registerIssueQuery(dbType interfaces.DbType) {
+	switch dbType {
+	case interfaces.Postgresql:
+		mustRegisterBean("IssueQuery", reflect.TypeFor[*issue_spec.IssueQueryImplPostgresql]())
+	case interfaces.Mysql:
+		mustRegisterBean("IssueQuery", reflect.TypeFor[*issue_spec.IssueQueryImplMysql]())
+	default:
+		mustRegisterBean("IssueQuery", reflect.TypeFor[*issue_spec.IssueQueryImpl]())
+	}
+}
+
+func registerServices() {
+	mustRegisterBean("JWTService", reflect.TypeFor[*services.JWTService]())
+	mustRegisterBean("PasswordService", reflect.TypeFor[*services.PasswordService]())
+	mustRegisterBean("LabelService", reflect.TypeFor[*app_services.LabelService]())
+}
+
+func registerUseCases() {
+	mustRegisterBean("AccountUseCases", reflect.TypeFor[*usecases.AccountUseCases]())
+	mustRegisterBean("ColumnUseCases", reflect.TypeFor[*usecases.ColumnUseCases]())
+	mustRegisterBean("TeamUseCases", reflect.TypeFor[*usecases.TeamUseCases]())
+	mustRegisterBean("LabelUseCases", reflect.TypeFor[*usecases.LabelUseCases]())
+	mustRegisterBean("UserUseCases", reflect.TypeFor[*usecases.UserUseCases]())
+	mustRegisterBean("GroupUseCases", reflect.TypeFor[*usecases.GroupUseCases]())
+	mustRegisterBean("ProjectUseCases", reflect.TypeFor[*usecases.ProjectUseCases]())
+	mustRegisterBean("SettingsUseCases", reflect.TypeFor[*usecases.SettingsUseCases]())
+	mustRegisterBean("SyncUseCases", reflect.TypeFor[*usecases.SyncUseCases]())
+	mustRegisterBean("KanbanUseCases", reflect.TypeFor[*usecases.KanbanUseCases]())
+	mustRegisterBean("SprintUseCases", reflect.TypeFor[*usecases.SprintUseCases]())
+	mustRegisterBean("IssueUseCases", reflect.TypeFor[*usecases.IssueUseCases]())
+	mustRegisterBean("IssueBindingUseCases", reflect.TypeFor[*usecases.IssueBindingUseCases]())
+	mustRegisterBean("ReleaseUseCases", reflect.TypeFor[*usecases.ReleaseUseCases]())
+	mustRegisterBean("EpicUseCases", reflect.TypeFor[*usecases.EpicUseCases]())
+}
+
+func registerControllers() {
+	mustRegisterBean("AccountController", reflect.TypeFor[*controllers.AccountController]())
+	mustRegisterBean("ReportsController", reflect.TypeFor[*controllers.ReportsController]())
+	mustRegisterBean("HealthController", reflect.TypeFor[*controllers.HealthController]())
+	mustRegisterBean("ColumnController", reflect.TypeFor[*controllers.ColumnController]())
+	mustRegisterBean("TeamController", reflect.TypeFor[*controllers.TeamController]())
+	mustRegisterBean("LabelController", reflect.TypeFor[*controllers.LabelController]())
+	mustRegisterBean("UserController", reflect.TypeFor[*controllers.UserController]())
+	mustRegisterBean("GroupController", reflect.TypeFor[*controllers.GroupController]())
+	mustRegisterBean("ProjectController", reflect.TypeFor[*controllers.ProjectController]())
+	mustRegisterBean("SettingsController", reflect.TypeFor[*controllers.SettingsController]())
+	mustRegisterBean("KanbanController", reflect.TypeFor[*controllers.KanbanController]())
+	mustRegisterBean("SprintController", reflect.TypeFor[*controllers.SprintController]())
+	mustRegisterBean("IssueController", reflect.TypeFor[*controllers.IssueController]())
+	mustRegisterBean("IssueBindingController", reflect.TypeFor[*controllers.IssueBindingController]())
+	mustRegisterBean("ReleaseController", reflect.TypeFor[*controllers.ReleaseController]())
+	mustRegisterBean("EpicController", reflect.TypeFor[*controllers.EpicController]())
+}
---
+++ backend/internal/infra/persistance/spec/issue/issue_filter_spec_mysql.go
@@ -0,0 +1,76 @@
+package issue_spec
+
+import (
+	"fmt"
+	"main/internal/app/queries"
+	"main/internal/domain/repo"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+type IssueFilterSpecMysql struct {
+	repo.QuerySpec
+	Filter queries.IssueFilter
+}
+
+func (s *IssueFilterSpecMysql) Apply(conn any) (any, error) {
+	query := conn.(*gorm.DB)
+
+	if s.Filter.AssigneeId != nil && s.Filter.SprintId == nil {
+		query = query.
+			Joins("left join assignees on issues.id = assignees.issue_id").
+			Where("assignees.user_id = ?", s.Filter.AssigneeId)
+	}
+	if s.Filter.GroupId != nil && s.Filter.SprintId == nil {
+		query = query.
+			Joins("left join assignees on issues.id = assignees.issue_id").
+			Joins("left join users on assignees.user_id = users.id").
+			Joins("left join user_groups on users.id = user_groups.user_id").
+			Where("user_groups.group_id = ?", s.Filter.GroupId)
+	}
+	if s.Filter.TeamId != nil && s.Filter.SprintId == nil {
+		query = query.
+			Joins("left join projects on issues.project_id = projects.id").
+			Where("projects.team_id = ?", s.Filter.TeamId)
+	}
+	if s.Filter.SprintId != nil {
+		query = query.
+			Select("issues.*, issue_bindings.id as binding_id").
+			Joins("left join issue_bindings on issues.id = issue_bindings.issue_id").
+			Where("issue_bindings.sprint_id = ?", s.Filter.SprintId)
+
+		if s.Filter.AssigneeId != nil {
+			query = query.
+				Joins("left join assignees on issues.id = assignees.issue_id").
+				Where("issue_bindings.assignee_id = @user_id OR (assignees.user_id = @user_id AND issue_bindings.assignee_id IS NULL)", map[string]any{
+					"user_id": s.Filter.AssigneeId,
+				})
+		}
+
+		if s.Filter.GroupId != nil {
+			query = query.
+				Joins("left join assignees on issues.id = assignees.issue_id").
+				Joins("left join users on assignees.user_id = users.id").
+				Joins("left join user_groups on user_groups.user_id = issue_bindings.assignee_id OR (user_groups.user_id = users.id AND issue_bindings.assignee_id IS NULL)").
+				Where("user_groups.group_id = ?", s.Filter.GroupId)
+		}
+	}
+	if s.Filter.ProjectId != nil {
+		query = query.Where("issues.project_id = ?", s.Filter.ProjectId)
+	}
+	if s.Filter.Search != nil {
+		searchText := strings.ToLower(*s.Filter.Search)
+		query = query.Where("(lower(issues.title) like ?) or (issues.iid = ?)", fmt.Sprintf("%%%s%%", searchText), searchText)
+	}
+
+	return query, nil
+}
+
+type IssueQueryImplMysql struct {
+	queries.IssueQuery
+}
+
+func (b *IssueQueryImplMysql) GetSpec(filter queries.IssueFilter) repo.QuerySpec {
+	return &IssueFilterSpecMysql{Filter: filter}
+}

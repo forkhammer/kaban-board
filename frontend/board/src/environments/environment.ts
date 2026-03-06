006f67ab85f3965c218b020fb60c98505ad2a660
Forkhammer <forkhammer@gmail.com>
2026-03-06 12:56:49 +0500

    caddy -> nginx

--- frontend/board/src/environments/environment.ts
+++ frontend/board/src/environments/environment.ts
@@ -3,6 +3,6 @@ const browserWindowEnv = browserWindow['env'] || {};
 
 export const environment = {
   isProduction: true,
-  apiUrl: browserWindowEnv.apiUrl,
+  apiUrl: browserWindowEnv.apiUrl || `${window.location.origin}/api`,
   autoUpdateIssuesMin: 2
 };
